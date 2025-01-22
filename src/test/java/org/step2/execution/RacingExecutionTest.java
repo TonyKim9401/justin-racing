48341997a2431a63e26bc509538443c5f2de5f37
ktony <k.tony9412@gmail.com>
2025-01-21 22:47:06 -0500

    레이싱 게임 실행에 대한 검증인데, 테스트 메소드의 반환 값은 void 이고 내부 로직은 RacingCars 객체와 CarLog 에 연관되어 있어 어떻게 테스트를 하면 좋을지 고민됨. 역할 부여가 제대로 되지 않은 생각도 들고, 실행시 제대로 실행 된건지 안된건지를 판단하는 기준이 불명확한것 같음

---
+++ src/test/java/org/step2/execution/RacingExecutionTest.java
@@ -0,0 +1,46 @@
+package org.step2.execution;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.step2.car.RacingCar;
+import org.step2.car.RacingCars;
+import org.step2.condition.RandomCondition;
+
+class RacingExecutionTest {
+
+    @DisplayName("레이싱 게임 실행 성공")
+    @Test
+    void testExecuteRacingGameSuccess() {
+        // given
+        RandomCondition mockRandomCondition = mock(RandomCondition.class);
+        when(mockRandomCondition.getConditionValue()).thenReturn(Boolean.TRUE);
+
+        Integer carCount = 2;
+        RacingCars racingCars = new RacingCars(carCount, mockRandomCondition);
+
+        Integer tryCount = 3;
+        RacingExecution racingExecution = new RacingExecution(racingCars, tryCount);
+
+        // when
+        racingExecution.executeRacingGame();
+
+        // then
+        List<RacingCar> cars = racingCars.getCars();
+
+        assertAll(
+                cars.stream()
+                        .map(car -> ()
+                                -> {
+                            assertThat(car.getCarLogs()).hasSize(tryCount);
+                            assertThat(car.getPosition()).isEqualTo(tryCount);
+//                            assertThat(car.getCarLogByTryCount(tryCount-1).getLog()).isEqualTo("---");
+                        })
+        );
+    }
+}
