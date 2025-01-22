28433f72fea46a3942f3f8b79c75319f656a568b
ktony <k.tony9412@gmail.com>
2025-01-21 22:45:18 -0500

    자바 시스템 내부의 출력값을 미리 셋팅하고 RandomCondition 을 목킹하여 원하는 출력 결과가 나오는지 확인

---
+++ src/test/java/org/step2/output/CarOutputTest.java
@@ -0,0 +1,58 @@
+package org.step2.output;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.step2.car.RacingCars;
+import org.step2.condition.RandomCondition;
+import org.step2.execution.RacingExecution;
+
+class CarOutputTest {
+
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @DisplayName("값 출력 성공")
+    @Test
+    void testPrintValueSuccess() {
+        // given
+        System.setOut(new PrintStream(outputStream));
+
+        RandomCondition mockRandomCondition = mock(RandomCondition.class);
+        when(mockRandomCondition.getConditionValue()).thenReturn(Boolean.TRUE);
+
+        Integer carCount = 2;
+        RacingCars racingCars = new RacingCars(carCount, mockRandomCondition);
+
+        Integer tryCount = 3;
+        RacingExecution racingExecution = new RacingExecution(racingCars, tryCount);
+        racingExecution.executeRacingGame();
+
+        CarOutput carOutput = new CarOutput(racingCars, tryCount);
+
+        // when
+        carOutput.printValue();
+
+        // then
+        String resultOutput = """
+                                실행 결과
+                                -
+                                -
+                                
+                                --
+                                --
+                                
+                                ---
+                                ---
+                                
+                                """;
+        assertThat(outputStream.toString()).isEqualTo(resultOutput);
+
+        System.setOut(originalOut);
+    }
+}
