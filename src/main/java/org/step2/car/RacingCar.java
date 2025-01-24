3239049fa714089cf6726fc7c692b1657b279921
ktony <k.tony9412@gmail.com>
2025-01-24 03:23:44 -0500

    RacingCar 클래스에 순서 저장용 carSequence 추가 및 차에 따라 실행 횟수를 저장하는 racingExecuteSequence 추가
    RacingCar 는 외부에서 생성 되므로 carSequence 를 생성자를 통해 초기화 하도록 수정
    
    moveForward 메소드 실행시 필요한 비즈로직을 메소드 단위로 분리

--- src/main/java/org/step2/car/RacingCar.java
+++ src/main/java/org/step2/car/RacingCar.java
@@ -10,7 +10,6 @@ import org.step2.condition.Condition;
  */
 public class RacingCar {
 
-    private Integer carRanking;
     private final Condition condition;
 
     /**
@@ -19,45 +18,46 @@ public class RacingCar {
      */
     private final List<CarLog> carLogs = new ArrayList<>();
 
+    private Integer carSequence;
     private Integer position;
+    private Integer racingExecuteSequence;
 
 
-    public RacingCar(Integer carRanking, Condition condition) {
-        this.carRanking = carRanking;
+    public RacingCar(Integer carSequence, Condition condition) {
+        this.carSequence = carSequence;
         this.condition = condition;
         this.position = 0;
-    }
-
-    /**
-     * CarRanking 이지만 실제로는 생성된 차량의 순서를 가지고 있음
-     * -> 이름 변경 필요
-     * -> 혹은 이후 각 실행마다 순위를 체크시 사용할수도 있지만, 순위는 CarLog 에 기록하는게 맞지 않을까?
-     */
-    public Integer checkCarRanking() {
-        return this.carRanking;
-    }
-
-    /**
-     * 각 RacingCar 의 실행 횟수의 결과를 가져옴
-     */
-    public CarLog getCarLogByTryCount(Integer tryCount) {
-        return this.carLogs.get(tryCount);
+        this.racingExecuteSequence = 0;
     }
 
     /**
      * condition 에 따라 이동 / 미이동이 결정 됨
      */
     public void moveForward() {
-        /**
-         * 테스트 코드 작성시
-         * 1. if 문에 따른 this.position 의 값 변화 비교
-         * 2. 메소드 실행시 카운트와 this.carLogs 의 사이즈 비교
-         * 로 진행하면 되지 않을까?
-         */
-        if (this.condition.getConditionValue()) {
-            this.position += 1;
-        }
-        this.carLogs.add(new CarLog(this.position));
+        checkMoveCondition();
+        addNewCarLog();
+        increaseRacingExecuteSequence();
+    }
+
+    private void checkMoveCondition() {
+        if (this.condition.getConditionValue()) this.position += 1;
+    }
+
+    private void addNewCarLog() {
+        this.carLogs.add(new CarLog(this.racingExecuteSequence, this.position));
+    }
+
+    private void increaseRacingExecuteSequence() {
+        this.racingExecuteSequence += 1;
+    }
+
+
+    public Integer checkCarSequence() {
+        return this.carSequence;
+    }
+
+    public CarLog getCarLogByTryCount(Integer tryCount) {
+        return this.carLogs.get(tryCount);
     }
 
     public List<CarLog> getCarLogs() {
@@ -66,5 +66,9 @@ public class RacingCar {
 
     public Integer getPosition() {
         return this.position;
+    }
+
+    public Integer getRacingExecuteSequence() {
+        return this.racingExecuteSequence;
     }
 }
